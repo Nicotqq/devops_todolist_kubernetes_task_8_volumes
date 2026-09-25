9913ba13cbd620a440d4d989c69d17e68b62a901
Mykola Hakh <kolyagakh@gmail.com>
2026-09-25 12:47:14 +0300

    Lesson complete

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+kubectl apply -f namespace.yml
+kubectl apply -f configMap.yml
+kubectl apply -f secret.yml
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
+kubectl apply -f hpa.yml
+kubectl apply -f clusterip.yml
+kubectl apply -f nodeport.yml
